c779413f5f4354fcce0f470b1b30ef92b64e1a7b
icon <soulsaber1@gmail.com>
2018-04-26 08:08:03 -0500

    mmm

---
+++ src/models.py
@@ -0,0 +1,25 @@
+from database import db
+from passlib.hash import sha256_crypt as sha256
+
+class UserModel(db.Model):
+  __tablename__ = 'users'
+
+  id = db.Column(db.Integer, primary_key = True)
+  username = db.Column(db.String(120), unique = True, nullable = False)
+  password = db.Column(db.String(120), nullable = False)
+    
+  def save_to_db(self):
+    db.session.add(self)
+    db.session.commit()
+  
+  @classmethod
+  def find_by_username(cls, username):
+    return cls.query.filter_by(username = username).first()
+
+  @staticmethod
+  def gen_hash(password):
+    return sha256.hash(password)
+
+  @staticmethod
+  def verify_hash(password, hash):
+    return sha256.verify(password, hash)
