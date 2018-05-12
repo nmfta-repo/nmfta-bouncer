eff750c7a5cd25375b8855dc4b2fb654642d0d2b
icon <soulsaber1@gmail.com>
2018-05-12 16:23:49 -0500

    add standards compliant src/firewall.py

--- src/firewall.py
+++ src/firewall.py
@@ -1,38 +1,41 @@
-from flask import Flask,jsonify
+"""Firewall module for the firewall app"""
+
+from flask import jsonify
+#import netaddr
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
+from flask_jwt_extended import jwt_required
 from models import BLModel, WLModel
-import netaddr
 
-parser = reqparse.RequestParser()
-parser.add_argument("IPv4", required=False)
-parser.add_argument("IPv6", required=False)
-parser.add_argument("Start_Date", required=False)
-parser.add_argument("End_Date", required=False)
-parser.add_argument("Comments", required=False)
-parser.add_argument("Active", required=False)
+PARSER = reqparse.RequestParser()
+PARSER.add_argument("IPv4", required=False)
+PARSER.add_argument("IPv6", required=False)
+PARSER.add_argument("Start_Date", required=False)
+PARSER.add_argument("End_Date", required=False)
+PARSER.add_argument("Comments", required=False)
+PARSER.add_argument("Active", required=False)
 
-''' 
-Check class allows switching between Whitelist and Blacklist
-from the Resource call by switching a Class variable from "wl" to "bl"
-This lets us use the same CRUD functions for Whitelist and Blacklist
-Maximizes code reuse while maintaining flexibility
-'''
-
-class Checker():
+class Checker(object): # pylint: disable=too-few-public-methods
+    """
+    Check class allows switching between Whitelist and Blacklist
+    from the Resource call by switching a Class variable from "wl" to "bl"
+    This lets us use the same CRUD functions for Whitelist and Blacklist
+    Maximizes code reuse while maintaining flexibility
+    """
     def __init__(self, ltype):
         self.ltype = ltype
     #check if request is WList or BList
     def type_test(self):
-        return WLModel if self.ltype is "wl" else BLModel
+        """Check if requester is looking for white/black list"""
+        return WLModel if self.ltype == "wl" else BLModel
 
-#List IP/Geo Entries
 class IpGeoList(Checker, Resource):
+    """IpGeoList Class"""
     @jwt_required
     def get(self):
+        """Handles get IpGeoList requests"""
         mod = self.type_test()
         return jsonify(
-            Result = {
+            Result={
                 "Status":"Success",
                 "Message":"Showing All IPs and Geo"
             },
@@ -40,95 +43,89 @@ class IpGeoList(Checker, Resource):
             GeoLocations=[]
         )
 
-#List IP Entries
 class IpList(Checker, Resource):
+    """IpList Class"""
     @jwt_required
     def get(self):
+        """Handles get IpList requests"""
         mod = self.type_test()
         return jsonify(
-            Result = {
+            Result={
                 "Status":"Success",
                 "Message":"Showing All IPs"
             },
             IPAddresses=mod.get_all_ip(),
         )
 
-#Search for List of Matching Entries
 class EntrySearch(Checker, Resource):
+    """EntrySearch Class"""
     @jwt_required
-    def get(self, filter):
+    def get(self, filter_term):
+        """Handles get EntrySearch requests"""
         mod = self.type_test()
         search_results = []
-        for ip in str(filter).split(","):
-            if "/" in ip:
+        for search_ip in str(filter_term).split(","):
+            if "/" in search_ip:
                 #do cidr calc
                 pass
-            t = mod.search(ip)
-            if t:
-                search_results.append(t.ipv4)
+            found_ip = mod.search(search_ip)
+            if found_ip:
+                search_results.append(found_ip.ipv4)
         return jsonify(
-            Result = {
+            Result={
                 "Status":"Success",
                 "Message":"Showing All Matching"
             },
-            SearchResult = {
-                "Input_IP":filter,
-                "Entries":search_results
-            }
-        )
+            SearchResult={
+                "Input_IP":filter_term,
+                "Entries":search_results})
 
-#Search for Specific Matching Entry
 class Entry(Checker, Resource):
+    """Entry Class"""
     @jwt_required
     def get(self, entry):
+        """Handles get Entry requests"""
         mod = self.type_test()
         info = mod.search(entry)
         if info is not None:
             return jsonify(
-                Result = {
+                Result={
                     "Status":"Success",
                     "Message":"Showing Matching Entry"
                 },
                 Entry={
-                "IPv4":info.ipv4, "IPv6":info.ipv6, "Start_Date":info.start_date,
+                    "IPv4":info.ipv4, "IPv6":info.ipv6, "Start_Date":info.start_date,
                     "End_Date":info.end_date, "Comments":info.comments, "Active":info.active}
             )
         return jsonify(
-            Result = {
+            Result={
                 "Status":"Error",
-                "Message":"No Matching Entry"
-            }
-        )
+                "Message":"No Matching Entry"})
 
-#Add entry to the Database
 class CreateIpEntry(Checker, Resource):
+    """CreateIpEntry Class"""
     @jwt_required
     def post(self):
+        """Handles post CreateIpEntry"""
         mod = self.type_test()
-        data = parser.parse_args()
-        ip = data["IPv4"] if data["IPv4"] else ""
-        ip = data["IPv6"] if data["IPv6"] else ip
-        if ip is "":
+        data = PARSER.parse_args()
+        entry_ip = data["IPv4"] if data["IPv4"] else ""
+        entry_ip = data["IPv6"] if data["IPv6"] else entry_ip
+        if entry_ip == "":
             return jsonify(
-                Result = {
+                Result={
                     "Status":"Invalid",
-                    "Message":"Must enter IPv4 or IPv6 address"
-                 }
-            )
+                    "Message":"Must enter IPv4 or IPv6 address"})
         #check if already in database
-        if BLModel.exists(ip) or WLModel.exists(ip):
+        if BLModel.exists(entry_ip) or WLModel.exists(entry_ip):
             return jsonify(
-                Result = {
+                Result={
                     "Status":"Error",
-                    "Message":"IP exists in system"
-                 }
-            )
-        new_ip = mod(ipv4 = data['IPv4'], ipv6 = data['IPv6'], start_date = data['Start_Date'],
-            end_date = data['End_Date'], comments = data['Comments'], active = data["Active"])
+                    "Message":"IP exists in system"})
+        new_ip = mod(ipv4=data['IPv4'], ipv6=data['IPv6'], start_date=data['Start_Date'],
+                     end_date=data['End_Date'], comments=data['Comments'], active=data["Active"])
         new_ip.save()
         return jsonify(
-                Result = {
-                        "Status":"Success",
-                        "Message":"IP Added"
-             }
-        )
+            Result={
+                "Status":"Success",
+                "Message":"IP Added"})
