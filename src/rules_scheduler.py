8310ca6800330a12d5e24a89a0a8350391f57420
icon <soulsaber1@gmail.com>
2018-06-09 19:08:42 -0500

    add python3 compliance, gui control app

--- src/rules_scheduler.py
+++ src/rules_scheduler.py
@@ -4,22 +4,20 @@ import sqlite3
 
 def dump_whitelists(conn):
     cur = conn.execute("SELECT id, ipv4 FROM whitelist")
-    print "Dumping Whitelist Entries:"
+    print("Dumping Whitelist Entries:")
     for row in cur:
-        print "id", row[0]
-        print "ipv4", row[1]
+        print("id", row[0], "ipv4", row[1])
 
 def dump_blacklists(conn):
     cur = conn.execute("SELECT id, ipv4 FROM blacklist")
-    print "Dumping Blacklist Entries:"
+    print("Dumping Blacklist Entries:")
     for row in cur:
-        print "id", row[0]
-        print "ipv4", row[1]
+        print("id", row[0], "ipv4", row[1])
 
 def main():
     conn = sqlite3.connect("src/firewall.db")
     dump_whitelists(conn)
-    print ""
+    print("")
     dump_blacklists(conn)
 
 if __name__ == "__main__":
