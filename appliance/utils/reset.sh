f66b1b5bc4e879c6623031637cf2897a70eff44a
icon <soulsaber1@gmail.com>
2018-06-24 20:23:37 -0500

    add configparser, default conf, argparsing

--- appliance/utils/reset.sh
+++ appliance/utils/reset.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -rf /home/marcus/.bouncer/
+mkdir /home/marcus/.bouncer/
+cp /home/marcus/firewall/default.conf /home/marcus/.bouncer/
+
 chmod +x /home/marcus/firewall/start.sh
 chmod +x /home/marcus/firewall/rules.sh
 cp /home/marcus/firewall/utils/bouncer-* /etc/systemd/system/
