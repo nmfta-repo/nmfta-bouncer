4b95ec96f06bb5c69a51dc2c3722b8d55aa73872
Drew <soulsaber1@gmail.com>
2018-07-17 15:15:15 -0500

    add functional login for client app

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-rsync -a -v --delete appliance/ marcus@192.168.0.11:firewall
-rsync -a -v --delete default.conf marcus@192.168.11:~/.bouncer/
+rsync -a -v --delete appliance/ marcus@$1:firewall
+rsync -a -v --delete default.conf marcus@$1:~/.bouncer/
+ssh marcus@$1 "chmod +x /home/marcus/firewall/utils/reset.sh"
