f8f0f7e8451d25fa68a0f9a62c732054dd762b58
Drew <soulsaber1@gmail.com>
2018-08-27 13:43:33 -0500

    add reworked scheduling engine to delete flagged entries

--- appliance/app/rules_scheduler.py
+++ appliance/app/rules_scheduler.py
@@ -14,20 +14,22 @@ def print_rules(conn):
     dump_blacklists(conn)
 
 def do_rules(conn):
-    cur = conn.execute("SELECT id, ipv4, remove FROM whitelist")
+    cur = conn.execute("SELECT id, ipv4, remove FROM iptable WHERE lt=\"wl\"")
     print("Creating Whitelist Rules:")
     for row in cur:
-        print("id", row[0], "ipv4", row[1])
         ufw.run("allow from {}".format(row[1]))
         if row[2]:
+            conn.execute("DELETE FROM iptable WHERE id={}".format(row[0]))
+            conn.commit()
             ufw.run("delete allow from {}".format(row[1]))
 
-    cur = conn.execute("SELECT id, ipv4, remove FROM blacklist")
+    cur = conn.execute("SELECT id, ipv4, remove FROM iptable WHERE lt='bl'")
     print("Creating Blacklist Rules:")
     for row in cur:
-        print("id", row[0], "ipv4", row[1])
         ufw.run("deny from {}".format(row[1]))
         if row[2]:
+            conn.execute("DELETE FROM iptable WHERE id={}".format(row[0]))
+            conn.commit()
             ufw.run("delete deny from {}".format(row[1]))
 
 def dump_whitelists(conn):
