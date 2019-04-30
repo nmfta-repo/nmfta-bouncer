2c4e58a32844e786004e14d34564d43a6fcd7cbe
Ben Gardiner <ben.l.gardiner@gmail.com>
2019-04-30 07:51:44 -0400

    leave grant type errors alone; make missing user and bad password the same (Krishna Kumar) -- issue #17

--- appliance/src/auth.py
+++ appliance/src/auth.py
@@ -48,7 +48,7 @@ class Login(Resource):
             return jsonify(Result={"Status":"Invalid","Error":"2001"})
         user = UserModel.lookup_user(data['username'])
         if data['grant_type'] != 'password':
-            return jsonify(Result={"Status":"Invalid","Error":"2003"})
+            return jsonify(Result={"Status":"Invalid","Error":"2002"})
         if not user:
             return jsonify(Result={"Status":"Invalid","Error":"2003"})
 
@@ -60,7 +60,7 @@ class Login(Resource):
                 expires_in=(minutes_valid*60),
                 claim_level="complete"
             )
-        return jsonify(message='failed to log in')
+        return jsonify(Result={"Status":"Invalid","Error":"2003"})
 
 class BrokenHTTPS(Resource):
     """BrokenHTTPS Class"""
